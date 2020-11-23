6e4dc07d79b5bca2e923ed8db936f5e890b4835b
jonathan-otr <armando-400@hotmail.com>
2020-11-23 12:56:40 -0600

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,6 @@
     <link rel="icon" href="{{asset('/img/logo.png')}}">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="{{ asset('js/smooth.js') }}" defer></script>
@@ -47,7 +46,7 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto" style="font-size:.8em!important;">
+                    <ul class="navbar-nav ml-auto" style="font-size:1.1em!important;">
                       <li class="nav-item">
                           <a class="nav-link whitecolor smooth" href="#smartkitchen"><b>{{ __('SMART KITCHEN') }}</b></a>
                       </li>
